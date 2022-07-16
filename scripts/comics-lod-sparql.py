88764a512be77c9e9d63b4b4bbd921553e5eb167
Sean Petiya <sean@petiya.com>
2022-07-15 23:14:52 -0400

    update sparql script, add topic example

--- scripts/comics-lod-sparql.py
+++ scripts/comics-lod-sparql.py
@@ -7,8 +7,29 @@ from rdflib import Graph
 g = Graph()
 g.parse("comics-lod.json")
 
+ns = {
+    "cbo": "https://comicmeta.org/cbo/",
+    "schema": "https://schema.org/",
+    "meddra": "http://purl.bioontology.org/ontology/MEDDRA/"
+}
+
 # query 1 - all content, all terms
-results = g.query('SELECT ?content ?about ?label WHERE { ?content schema:about ?about . ?about schema:name ?label . }', initNs={ 'cbo': 'https://comicmeta.org/cbo/', 'schema': 'https://schema.org/' })
+
+results = g.query("""
+   SELECT ?content ?about ?label 
+    WHERE { 
+        ?content schema:about ?about . 
+        ?about schema:name ?label . 
+    }""", initNs=ns)
+
+
+# query 2 - content about panic attacks (meddra:10033664)
+
+# results = g.query("""
+#   SELECT ?content
+#   WHERE {
+#        ?content schema:about meddra:10033664 .
+#    }""", initNs=ns)
 
 for row in results:
     print(f"{row.content} {row.about} ({row.label})")
